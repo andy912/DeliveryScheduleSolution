b9c3094e6876386fcf9834ea7ff0af15a345c633
Andy <andywu0912@gmail.com>
2025-10-20 20:03:30 +0800

    SQL

---
+++ Services/RoleService.cs
@@ -0,0 +1,22 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace DeliveryScheduleSolution.Services
+{
+    public class RoleService
+    {
+        private readonly string _connectionString;
+        public RoleService(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("DefaultConnection")
+                ?? throw new ArgumentNullException("Connection string 'DefaultConnection' not found.");
+        }
+
+        public async Task<List<string>> GetRolesAsync()
+        {
+            using var conn = new SqlConnection(_connectionString);
+            var sql = "SELECT RoleName FROM Roles ORDER BY RoleName";
+            return (await conn.QueryAsync<string>(sql)).ToList();
+        }
+    }
+}
